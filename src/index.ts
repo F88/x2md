5730cfcfd9080080cee03dbfc8e6c9506db2118b
F88 <685250+F88@users.noreply.github.com>
2025-06-02 22:47:11 +0900

    docs: add package documentation for utility functions in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 /**
+ * Collection of tiny utilities that convert input values into strings suitable for Markdown formatting.
+ *
+ * @packageDocumentation
+ */
+
+/**
  * This is a simple function that returns a greeting message.
  *
  * @public
